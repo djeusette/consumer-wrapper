7ab5fd1543ce4f4c787c0f57c6e460618c262ee0
David Jeusette <jeusette.david@gmail.com>
2015-07-06 22:15:09 +0200

    Add consumer to exports

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@
 "use strong";
 
 let ConsumerWrapper = require("./consumer_wrapper")
+let Consumer        = require("./app/consumers/consumer")
+let AmqpConsumer    = require("./app/consumers/amqp_consumer")
 
-module.exports = ConsumerWrapper
+module.exports              = ConsumerWrapper
+module.exports.Consumer     = Consumer
+module.exports.AmqpConsumer = AmqpConsumer
